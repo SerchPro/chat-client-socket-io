6f8bb9baa1c172d4d069eca2211228c27d76288d
SerchPro <proyects_serch@hotmail.com>
2022-06-02 11:52:57 -0500

    create routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import {
-  Navigate,
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
 } from 'react-router-dom';
-import { LoginPage } from '../pages/LoginPage';
-import { RegisterPage } from '../pages/RegisterPage';
+
 import { ChatPage } from '../pages/ChatPage';
+import { AuthRouter } from './AuthRouter';
  
 export const AppRouter = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes path='/' element={<ChatPage />}>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/' element={<ChatPage />} />
-          {/*     
-          <Navigate to='/' />   
-          */}
+        <Routes >
+          <Route exact path='/' element={<ChatPage />} />
+          <Route path='/auth/*' element={<AuthRouter />} />
+          <Route path = '*' element = { <Navigate replace to = '/' />} />
+
         </Routes>
       </BrowserRouter>
     </>
