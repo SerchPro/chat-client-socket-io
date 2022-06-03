bb595f29c52298b3ea2c5ed355801b165e16d475
SerchPro <proyects_serch@hotmail.com>
2022-06-03 13:35:18 -0500

    add private and public routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
   BrowserRouter,
   Routes,
   Route,
-  Navigate,
 } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
 
 import { ChatPage } from '../pages/ChatPage';
 import { AuthRouter } from './AuthRouter';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
  
 export const AppRouter = () => {
+
+  const  { auth, verifyToken} = useContext(AuthContext);
+
+
+  useEffect(() => {
+    verifyToken()
+  }, [verifyToken]);
+
+  if(auth.checking){
+    return <h1> Please wait...</h1>
+  }
+  
   return (
     <>
       <BrowserRouter>
-        <Routes >
-          <Route exact path='/' element={<ChatPage />} />
-          <Route path='/auth/*' element={<AuthRouter />} />
-          <Route path = '*' element = { <Navigate replace to = '/' />} />
-
-        </Routes>
-      </BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <PublicRoute isAuthenticated={auth.logged}>
+                            <ChatPage />
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    path="/auth/*"
+                    element={
+                        <PrivateRoute isAuthenticated={auth.logged}>
+                            <AuthRouter />
+                        </PrivateRoute>
+                    }
+                />
+ 
+                <Route path="*" element={<ChatPage />} />
+            </Routes>
+        </BrowserRouter>
     </>
   );
 };
