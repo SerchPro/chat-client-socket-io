bc6c30996e83a05b8779b49aeb5040a15843487a
SerchPro <proyects_serch@hotmail.com>
2022-06-03 12:06:44 -0500

    add fetchtoken function

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,4 +1,5 @@
 const baseUrl = process.env.REACT_APP_API_URL;
+const token = localStorage.getItem('token') || undefined
 
 export const fetchNotoken = async (endpoint, data, method = 'GET') =>{
     console.log(endpoint, data, method)
@@ -12,6 +13,33 @@ export const fetchNotoken = async (endpoint, data, method = 'GET') =>{
             method,
             headers:{
                 'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        return await resp.json();
+    }
+
+}
+
+
+export const fetchtoken = async (endpoint, data, method = 'GET') =>{
+    console.log(endpoint, data, method)
+    const url = `${ baseUrl }/${ endpoint }`;
+
+    if(method === 'GET'){
+        const resp = await fetch( url, { 
+            headers: {
+                'x-token': token
+            }
+        } );
+        return await resp.json();
+    }else{
+        const resp = await fetch( url, {
+            method,
+            headers:{
+                'Content-type': 'application/json',
+                'x-token': token
             },
             body: JSON.stringify(data)
         })
