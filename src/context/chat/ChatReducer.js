bf7b8ebdc5242766ff35390cfe051d747b02c5ef
SerchPro <proyects_serch@hotmail.com>
2022-06-04 16:47:18 -0500

    add chatContext and chatReducer

---
+++ src/context/chat/ChatReducer.js
@@ -0,0 +1,14 @@
+import { types } from "../../types/types";
+
+export const ChatReducer = ( state, action ) => {
+
+    switch (action.type) {
+        case types.uploadedUsers:
+            return {
+                ...state,
+                users: [action.payload]
+            }
+        default:
+            state;
+    }
+}
