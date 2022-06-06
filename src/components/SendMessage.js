d1eaeaa1640bbecefd183d5951d871fed35d7c28
SerchPro <proyects_serch@hotmail.com>
2022-06-06 13:24:44 -0500

    get info form the form

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const SendMessage = () => {
-  return (
-      <form>
-        <div className="type_msg row">
-            <div className="input_msg_write col-sm-9">
-                <input type="text" className="write_msg" placeholder="Mensaje..." />
-            </div>
-            <div className="col-sm-3 text-center">
-                <button className="msg_send_btn mt-3" type="submit">
-                    send
-                </button>
-            </div>
-        </div>
-      </form>
 
-  )
+    const [message, setMessage] = useState('');
+
+    const onChange = ({ target }) => {
+        setMessage( target.value );
+    }
+    
+    const onSubmit = (ev) =>{
+        ev.preventDefault();
+
+        if( message.length === 0 ){ return;}
+        setMessage('')
+    }
+    return (
+        <form onSubmit={ onSubmit }>
+            <div className="type_msg row">
+                <div className="input_msg_write col-sm-9">
+                    <input 
+                        type="text" 
+                        className="write_msg" 
+                        placeholder="Mensaje..." 
+                        value={ message}
+                        onChange = { onChange}
+                        />
+                </div>
+                <div className="col-sm-3 text-center">
+                    <button className="msg_send_btn mt-3" type="submit">
+                        send
+                    </button>
+                </div>
+            </div>
+        </form>
+
+    )
 }
