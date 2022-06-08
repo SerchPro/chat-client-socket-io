c6a0d4e1149c635388f2e661f1d24cc3ff67cb53
SerchPro <proyects_serch@hotmail.com>
2022-06-07 22:29:33 -0500

    render new messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../auth/AuthContext'
+import { ChatContext } from '../context/chat/ChatContext'
 import { IncomingMessage } from './IncomingMessage'
 import { OutgoingMessage } from './OutgoingMessage'
 import { SendMessage } from './SendMessage'
 
 export const Messages = () => {
+
+  const { chatState } = useContext( ChatContext );
+  const { auth } = useContext( AuthContext );
+
   return (
-            <div className="mesgs">
+            <div
+                id="mensajes"
+                className="mesgs">
 
                 <div className="msg_history">
-
-                    <IncomingMessage/>
-
-                    <OutgoingMessage/>
+                  {
+                    chatState.messages.map(msg => (
+                      ( msg.to === auth.uid )
+                        ? <IncomingMessage key={ msg._id }  msg = { msg }/>
+                        : <OutgoingMessage key={ msg._id }  msg = { msg }/>
+                    ))
+                  }                  
 
                 </div>
 
